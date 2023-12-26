75a5a87a4fdbb167c0ea3e940bcfa5c71592384c
NaiDang <Naid4243@gmail.com>
2023-12-26 12:35:49 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import React, { useState, useEffect } from "react";
+// import './App.css';
+// import AddForm from "./assets/components/Home";
+// function App() {
+//   return (
+//       <AddForm></AddForm>
+//   );
+// }
+// export default App;
+
+import React from 'react';
+import Home from './assets/components/Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
